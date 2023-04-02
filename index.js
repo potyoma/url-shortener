fee4ac3ce90ddcd4b80e88f9a0ab1ca82b6c2b7d
Artyom Novoselov <an@versta24.ru>
2023-04-02 22:51:02 +0400

    format

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const {resolve} = require("node:dns")
+const { resolve } = require("node:dns")
 const bodyparser = require("body-parser")
 const app = express();
 
@@ -12,16 +12,16 @@ const urls = []
 
 app.use(cors());
 
-app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
@@ -32,18 +32,18 @@ app.get("/api/shorturl/:short_url", (req, res) => {
 })
 
 app.post("/api/shorturl", (req, res) => {
-  const {url} = req.body
+  const { url } = req.body
   const isValid = url.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/)
 
   if (!isValid) {
-    res.json({error: "invalid url"})
+    res.json({ error: "invalid url" })
     return
   }
-  
-    urls.push(url)
-    res.json({original_url: url, short_url: urls.length - 1})
+
+  urls.push(url)
+  res.json({ original_url: url, short_url: urls.length - 1 })
 })
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
